1fabac8e6d92a0e2695b96e1238c2150074901f5
Karan Sharma <karan1276@gmail.com>
2016-02-10 16:19:38 +0530

    Create prac 7 folder Start to make a calculator using javafx and scene builder.

---
+++ 7 Seventh pracs/MathFX/src/mathfx/FXMLDocumentController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mathfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author student
+ */
+public class FXMLDocumentController implements Initializable {
+    int n1,n2;
+    @FXML
+    private Button btadd;
+    @FXML
+    private Button btsub;
+    @FXML
+    private Button btmult;
+    @FXML
+    private Button btdiv;
+    @FXML
+    private Button btchng;
+    @FXML
+    private Button btcalc;
+    @FXML
+    private TextField num1;
+    @FXML
+    private TextField num2;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+       
+        if(btadd== e.getSource()){
+            n1 = Integer.parseInt(num1.getText());
+            n2 = Integer.parseInt(num2.getText());
+            System.out.println("Addintion is :"+(n1+n2));
+        }
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
